776aab59bab0e4aa8194af1a5156d3795285f848
cjp666 <chris.j.podmore@gmail.com>
2016-08-05 14:00:28 +0100

    using es7 decorators

--- app.js
+++ app.js
@@ -1,25 +1,18 @@
+import {inject} from 'aurelia-framework';
 import {HttpClient} from 'aurelia-http-client';
 
+@inject(HttpClient)
 export class App {
     constructor(httpClient) {
         this.message = '';
         this.http = httpClient;
     }
 
-    static inject() {
-        return [ HttpClient ];
-    }
-
     activate() {
-        this.message = 'Hello from Aurelia!';
         return this.http
             .get('/movies.json')
             .then(response => {
                 this.movies = response.content;
             });
-    }
-
-    changeMessage() {
-        this.message = 'Goodbye from Aurelia!';
     }
 }
